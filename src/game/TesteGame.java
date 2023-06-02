c7240525725b82e87039310e9f596fe8ba638e35
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-06-02 17:22:11 -0300

    suubindo exercícios programação orientada objetos

--- src/game/TesteGame.java
+++ src/game/TesteGame.java
@@ -7,7 +7,7 @@ public class TesteGame {
 		Produto game1 = new Produto(1, "The Last Of Us", "Playstation", 3, 500.00);
 		game1.visualizar();
 		
-		Produto game2 = new Produto(2, "Sonic", "Nintendo", 7, 1.000);
+		Produto game2 = new Produto(2, "Sonic", "Nintendo", 7, 100.00);
 		game2.visualizar();
 	}
 }
